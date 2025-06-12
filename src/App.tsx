946d606551f38bccb98de258620fb571b220baf7
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-06-12 15:40:16 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import Accordion from "./components/Accordion/Accordion.tsx";
+import {Rating} from "./components/Rating/Rating.tsx";
+
+
+function App() {
+    return (
+        <div>
+            <PageTitle title={'This is App Title'}/>
+            <Accordion titleValue={'Users'} collapsed={false}/>
+            <Rating value={5}/>
+            <Accordion titleValue={'Menu'} collapsed={true}/>
+            <Rating value={2}/>
+        </div>
+    )
+}
+type PageTitlePropsType = {
+    title: string,
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    return <h1>{props.title}</h1>
+}
+
+export default App
